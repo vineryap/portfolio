011214e5878d96a5b6904d4fc5870c9151e99496
vineryap <vineryap@gmail.com>
2021-09-01 16:15:26 +0900

    add tailwindcss typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   mode: 'jit',
-  purge: ['./public/**/*.html', './src/**/*.{astro,js,jsx,svelte,ts,tsx,vue}'],
-  plugins: [require('daisyui')],
+  purge: ['./public/**/*.{html}', './src/**/*.{astro,js,jsx,svelte,ts,tsx,vue}'],
+  plugins: [require('@tailwindcss/typography'), require('daisyui')],
 };
