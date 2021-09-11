91458b9ecb4554448d28413e7baa535dee88f52d
vineryap <vineryap@gmail.com>
2021-09-11 11:57:32 +0900

    changed to typescript

--- src/apis/quotes.ts
+++ src/apis/quotes.ts
@@ -1,4 +1,4 @@
-export async function fetchQuotes() {
+export async function fetchQuotes(): Promise<object> {
   return await fetch(
     `https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json`
   )
