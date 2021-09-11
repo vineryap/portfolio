af2c878a11f9dac44d55f8d584ab2888c1db41e6
vineryap <vineryap@gmail.com>
2021-09-11 12:06:40 +0900

    update snowpack config

--- snowpack.config.mjs
+++ snowpack.config.mjs
@@ -1,3 +1,22 @@
 export default {
-  plugins: ['@snowpack/plugin-postcss'],
+  alias: {
+    '~': './src',
+    $apis: './src/apis',
+    $components: './src/components',
+    $layouts: './src/layouts',
+    $stores: './src/stores',
+    $styles: './src/styles',
+    $utils: './src/utils',
+  },
+  mount:{
+    src: '/src',
+    public: '/public'
+  },
+  plugins: ['@snowpack/plugin-dotenv'],
+  packageOptions: {
+    rollup: {
+      plugins: [
+      ],
+    },
+  },
 };
